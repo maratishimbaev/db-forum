2db7bc98aada579d817164db05d05ace3c5a17ce
maratishimbaev <maratishimbaev8@gmail.com>
2020-03-23 17:04:28 +0500

    Added user delivery

--- user/delivery/http/handler.go
+++ user/delivery/http/handler.go
@@ -1,1 +1,61 @@
 package http
+
+import (
+	"forum/models"
+	"forum/user"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Handler struct {
+	useCase user.UseCase
+}
+
+func NewHandler(useCase user.UseCase) *Handler {
+	return &Handler{useCase: useCase}
+}
+
+func (h *Handler) CreateUser(c *gin.Context) {
+	newUser := models.User{
+		Nickname: c.Param("nickname"),
+	}
+
+	if err := c.BindJSON(newUser); err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+
+	user, err := h.useCase.CreateUser(&newUser)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+func (h *Handler) GetUser(c *gin.Context) {
+	nickname := c.Param("nickname")
+
+	user, err := h.useCase.GetUser(nickname)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+func (h *Handler) ChangeUser(c *gin.Context) {
+	newUser := models.User{
+		Nickname: c.Param("nickname"),
+	}
+
+	if err := c.BindJSON(newUser); err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+
+	user, err := h.useCase.ChangeUser(&newUser)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+
+	c.JSON(http.StatusOK, user)
+}
