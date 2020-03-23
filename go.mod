76a8b5f6486b57e23c71790e60f419f52c9f5637
maratishimbaev <maratishimbaev8@gmail.com>
2020-03-23 14:35:59 +0500

    Added repository template

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main.go
+module forum
 
 go 1.13
--- user/repository/postgres/user.go
+++ user/repository/postgres/user.go
@@ -1,1 +1,56 @@
 package postgres
+
+import (
+	"forum/models"
+)
+
+type Repository struct {}
+
+func NewRepository() *Repository {
+	return &Repository{}
+}
+
+type User struct {
+	About string
+	Email string
+	FullName string
+	Nickname string
+}
+
+func toPostgresUser(user models.User) *User {
+	return &User{
+		About:    user.About,
+		Email:    user.Email,
+		FullName: user.FullName,
+		Nickname: user.Nickname,
+	}
+}
+
+func toPostgresUserUpdate(user models.UserUpdate) *User {
+	return &User{
+		About:    user.About,
+		Email:    user.Email,
+		FullName: user.FullName,
+	}
+}
+
+func toModel(user User) *models.User {
+	return &models.User{
+		About:    user.About,
+		Email:    user.Email,
+		FullName: user.FullName,
+		Nickname: user.Nickname,
+	}
+}
+
+func (r *Repository) CreateUser(newUser *models.User) (user models.User, err error) {
+	return user, err
+}
+
+func (r *Repository) GetUser(nickname string) (user models.User, err error) {
+	return user ,err
+}
+
+func (r *Repository) ChangeUser(newUser *models.User) (user models.User, err error) {
+	return user, err
+}
