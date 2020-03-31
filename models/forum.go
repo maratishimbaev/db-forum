8e2b1414099228bbe42e98b75d0f23aafd83f4a1
maratishimbaev <maratishimbaev8@gmail.com>
2020-03-31 13:21:29 +0500

    Some changes

--- models/forum.go
+++ models/forum.go
@@ -1,9 +1,9 @@
 package models
 
 type Forum struct {
-	Posts uint64 `json:"posts"`
-	Slug string `json:"slug"`
-	Threads uint64 `json:"threads"`
-	Title string `json:"title"`
-	User string `json:"user"`
+	Posts uint64 `json:"posts,omitempty"`
+	Slug string `json:"slug,omitempty"`
+	Threads uint64 `json:"threads,omitempty"`
+	Title string `json:"title,omitempty"`
+	User string `json:"user,omitempty"`
 }
